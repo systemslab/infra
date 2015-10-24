98843611374e848dbfa373feee201b1f63613ed4
Michael Sevilla <mikesevilla3@gmail.com>
2015-10-23 23:50:42 -0700

    Deploy zlog but need to figure out how to not overwrite rados-classes:
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- bin/emaster.sh
+++ bin/emaster.sh
@@ -16,12 +16,29 @@ if [ "$?" -ne 0 ]; then
   echo "... wrong password? Try again."
   exit 
 fi
-
 docker rm emaster
+
 
 echo "==============================================================================="
 echo "You are now in an experiment master shell!"
+cat << "EOF"
+ _____                      _                      _     __  __           _            
+| ____|_  ___ __   ___ _ __(_)_ __ ___   ___ _ __ | |_  |  \/  | __ _ ___| |_ ___ _ __ 
+|  _| \ \/ / '_ \ / _ \ '__| | '_ ` _ \ / _ \ '_ \| __| | |\/| |/ _` / __| __/ _ \ '__|
+| |___ >  <| |_) |  __/ |  | | | | | | |  __/ | | | |_  | |  | | (_| \__ \ ||  __/ |   
+|_____/_/\_\ .__/ \___|_|  |_|_| |_| |_|\___|_| |_|\__| |_|  |_|\__,_|___/\__\___|_|   
+           |_|                                                                         
+EOF
 echo "==============================================================================="
+docker run -it \
+  --name="emaster" \
+  --net=host \
+  --volume="$(dirname `pwd`):/hathisar-dev/" \
+  --workdir="/hathisar-dev/experiments" \
+  michaelsevilla/emaster \
+  cat /etc/*release
+docker rm emaster
+
 docker run -it \
   --name="emaster" \
   --net=host \

28890b3320adbd8ad537ac68664a738b56f89882
Michael Sevilla <mikesevilla3@gmail.com>
2015-11-02 13:48:37 -0800

    Separate cluster/localhost and fix tachyon build/deploy
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-docker ps -aq | xargs docker rm -f
+docker ps -aq | xargs docker rm -f >> /dev/null 2>&1
 if [ "$1" == "remove" ]; then
-    docker images -aq | xargs docker rmi -f
-    sudo rm -r /tmp/docker
+    docker images -aq | xargs docker rmi -f >> /dev/null 2>&1
+    sudo rm -r /tmp/docker >> /dev/null 2>&1
 fi
 
 sudo rm -r /etc/ceph/* >> /dev/null 2>&1
+exit 0
--- bin/emaster.sh
+++ bin/emaster.sh
@@ -1,21 +1,49 @@
 #! /bin/bash
 
-# clear out existing docker containers
+# Ascii art: http://www.network-science.de/ascii/; font is "big"
+function fail() {
+  echo "==============================================================================="
+cat << "EOF"
+ ______      _  _            _ 
+|  ____|    (_)| |          | |
+| |__  __ _  _ | |  ___   __| |
+|  __|/ _` || || | / _ \ / _` |
+| |  | (_| || || ||  __/| (_| |
+|_|   \__,_||_||_| \___| \__,_|
+EOF
+  echo "==============================================================================="
+  exit 1
+}
+
+if [ -z "$1" ]; then
+  CLUSTER="localhost"
+else
+  CLUSTER="$1"
+fi
+echo "===> Experiment master will control all hosts listed in $CLUSTER"
+
+if [ ! -d "../experiments/$CLUSTER" ]; then
+  echo "===> Couldn't find directory with hosts listed in ../experiments/$CLUSTER"
+  fail
+fi
+
+echo "===> Cleaning up old docker containers - this may require a sudo password"
 ./cleanup.sh >> /dev/null 2>&1
-mkdir /tmp/docker >> /dev/null 2>&1
+mkdir -p /tmp/docker/src /tmp/docker/deploy >> /dev/null 2>&1
+
 echo "===> Installing an Ansible Docker container and dropping you into an 'experiment shell'"
 docker run -it --rm \
   --name="emaster" \
   --net=host \
   --volume="$(dirname `pwd`):/infra/" \
-  --workdir="/infra/experiments" \
+  --workdir="/infra/experiments/$CLUSTER" \
   --privileged \
   michaelsevilla/emaster \
-  ansible-playbook -k ../roles/emaster/tasks/pushkeys.yml
+  ansible-playbook -k ../../roles/emaster/tasks/pushkeys.yml
 
 if [ "$?" -ne 0 ]; then
-  echo "... wrong password? Try again."
-  exit 
+  echo "===> ... wrong password? Try again."
+  fail
 fi
 
 echo "===> Here are the specs of your environment:"
@@ -25,14 +53,20 @@ docker run -it --rm \
   cat /etc/*release
 
 echo "==============================================================================="
-echo "===> ou are now in an experiment master shell!"
+echo "===> You are now in an experiment master shell!"
 cat << "EOF"
- _____                      _                      _     __  __           _            
-| ____|_  ___ __   ___ _ __(_)_ __ ___   ___ _ __ | |_  |  \/  | __ _ ___| |_ ___ _ __ 
-|  _| \ \/ / '_ \ / _ \ '__| | '_ ` _ \ / _ \ '_ \| __| | |\/| |/ _` / __| __/ _ \ '__|
-| |___ >  <| |_) |  __/ |  | | | | | | |  __/ | | | |_  | |  | | (_| \__ \ ||  __/ |   
-|_____/_/\_\ .__/ \___|_|  |_|_| |_| |_|\___|_| |_|\__| |_|  |_|\__,_|___/\__\___|_|   
-           |_|                                                                         
+ ______                           _                          _   
+|  ____|                         (_)                        | |  
+| |__   __  __ _ __    ___  _ __  _  _ __ ___    ___  _ __  | |_ 
+|  __|  \ \/ /| '_ \  / _ \| '__|| || '_ ` _ \  / _ \| '_ \ | __|
+| |____  >  < | |_) ||  __/| |   | || | | | | ||  __/| | | || |_ 
+|______|/_/\_\| .__/  \___||_|   |_||_| |_| |_| \___||_| |_| \__|
+ _    _       |_|                                                
+|  \/  |             _              
+| \  / |  __ _  ___ | |_  ___  _ __ 
+| |\/| | / _` |/ __|| __|/ _ \| '__|
+| |  | || (_| |\__ \| |_|  __/| |   
+|_|  |_| \__,_||___/ \__|\___||_|   
 EOF
 echo "==============================================================================="
 docker run -it --rm \
@@ -41,7 +75,7 @@ docker run -it --rm \
   --volume="$(dirname `pwd`):/infra/" \
   --volume="/tmp/:/tmp/" \
   --volume="/etc/ceph:/etc/ceph" \
-  --workdir="/infra/experiments" \
+  --workdir="/infra/experiments/$CLUSTER" \
   --volume="/var/run/docker.sock:/var/run/docker.sock" \
   --privileged \
   michaelsevilla/emaster \

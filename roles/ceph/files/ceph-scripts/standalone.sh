823542c40ddc1babccac8deebd3c21d82d038961
Michael Sevilla <mikesevilla3@gmail.com>
2015-11-09 17:47:59 -0800

    ceph: custom startup scripts
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- roles/ceph/files/ceph-scripts/standalone.sh
+++ roles/ceph/files/ceph-scripts/standalone.sh
@@ -8,6 +8,7 @@ set -e
 
 CEPH_NETWORK=127.0.0.1/24
 MON_IP=127.0.0.1
+OSDs=6
 
 CEPH_OPTS="--cluster ${CLUSTER}"
 
@@ -97,19 +98,20 @@ ceph ${CEPH_OPTS} osd pool set rbd size 1
 #######
 # OSD #
 #######
+for i in `seq 0 $(($OSDs - 2))`; do
+  if [ ! -e /var/lib/ceph/osd/${CLUSTER}-$i/keyring ]; then
+    # bootstrap OSD
+    mkdir -p /var/lib/ceph/osd/${CLUSTER}-$i
+    ceph ${CEPH_OPTS} osd create
+    ceph-osd ${CEPH_OPTS} -i $i --mkfs
+    ceph ${CEPH_OPTS} auth get-or-create osd.$i osd 'allow *' mon 'allow profile osd' -o /var/lib/ceph/osd/${CLUSTER}-$i/keyring
+    ceph ${CEPH_OPTS} osd crush add $i 1 root=default host=$(hostname -s)
+    ceph-osd ${CEPH_OPTS} -i $i -k /var/lib/ceph/osd/${CLUSTER}-0/keyring
+  fi
 
-if [ ! -e /var/lib/ceph/osd/${CLUSTER}-0/keyring ]; then
-  # bootstrap OSD
-  mkdir -p /var/lib/ceph/osd/${CLUSTER}-0
-  ceph ${CEPH_OPTS} osd create
-  ceph-osd ${CEPH_OPTS} -i 0 --mkfs
-  ceph ${CEPH_OPTS} auth get-or-create osd.0 osd 'allow *' mon 'allow profile osd' -o /var/lib/ceph/osd/${CLUSTER}-0/keyring
-  ceph ${CEPH_OPTS} osd crush add 0 1 root=default host=$(hostname -s)
-  ceph-osd ${CEPH_OPTS} -i 0 -k /var/lib/ceph/osd/${CLUSTER}-0/keyring
-fi
-
-# start OSD
-ceph-osd ${CEPH_OPTS} -i 0
+  # start OSD
+  ceph-osd ${CEPH_OPTS} -i $i
+done
 
 
 #######

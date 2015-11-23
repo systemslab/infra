fcd613dab5845723204bb8342d11abadd69f5b0a
Michael Sevilla <mikesevilla3@gmail.com>
2015-11-23 14:01:28 -0800

    lttng: add java x11 lttng: new update to lttng2.7 breaks channels, need to downgrade to pre 11/16 lttng

--- bin/emaster.sh
+++ bin/emaster.sh
@@ -20,6 +20,11 @@ if [ -z "$1" ]; then
 else
   CLUSTER="$1"
 fi
+
+echo "===> Figure out which screen to use"
+SCREEN=`echo $DISPLAY | sed s/localhost//g | sed 's/\.0//g'`
+XAUTH=`xauth list | grep $SCREEN`
+
 echo "===> Experiment master will control all hosts listed in $CLUSTER"
 ARGS="-it --rm \
       --name=\"emaster\"\
@@ -30,9 +35,10 @@ ARGS="-it --rm \
       --volume=\"/var/run/docker.sock:/var/run/docker.sock\" \
       --volume=\"$(dirname `pwd`):/infra\" \
       --workdir=\"/infra/experiments/localhost\" \
-      -e DISPLAY=$DISPLAY \
-      -v /tmp/.X11-unix:/tmp/.X11-unix \
       --privileged"
+
+      #-e DISPLAY=$DISPLAY \
+      #-v /tmp/.X11-unix:/tmp/.X11-unix \
 
 if [ ! -d "../experiments/$CLUSTER" ]; then
   echo "===> Couldn't find directory with hosts listed in ../experiments/$CLUSTER"
@@ -68,5 +74,6 @@ cat << "EOF"
 EOF
 echo "==============================================================================="
 echo "===> Here are the specs of your environment:"
-docker run $ARGS michaelsevilla/emaster cat /etc/lsb-release /etc/os-release
-docker run $ARGS michaelsevilla/emaster /bin/bash
+docker run $ARGS michaelsevilla/emaster cat /etc/lsb-release /etc/os-release | while read p; do echo -e "\t $p"; done
+#docker run $ARGS -e "XAUTH=$XAUTH" michaelsevilla/emaster
+docker run $ARGS -e "XAUTH=$XAUTH" michaelsevilla/emaster /bin/emaster-shell
